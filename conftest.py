6e94ba1b0f2f283dfe1a7afd61678439ec0692fe
Evelina <politko96@inbox.ru>
2026-04-22 21:24:54 +0300

    fix: docker + selenium + allure report generation in CI

--- conftest.py
+++ conftest.py
@@ -1,15 +1,21 @@
 import pytest
-from requests import options
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
 @pytest.fixture(scope="function", autouse=True)
 def driver(request):
     options = Options()
-    options.add_argument("--no-sandox")
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
-    driver = webdriver.Chrome(options=options)
+    options.add_argument("--disable-gpu")
+    options.binary_location = "/usr/bin/chromium"
+
+    service = Service("/usr/bin/chromedriver")
+    driver = webdriver.Chrome(service=service, options=options)
+
     request.cls.driver = driver
     yield driver
     driver.quit()
